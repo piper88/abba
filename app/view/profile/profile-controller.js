3b8d16d0d786ad40a4bb0c01fc083a2973aa09b9
Sarah DeBey <sarah.debey@gmail.com>
2016-11-15 15:46:07 -0800

    everything is broken

--- app/view/profile/profile-controller.js
+++ app/view/profile/profile-controller.js
@@ -3,9 +3,21 @@
 require('./_profile.scss');
 
 
-module.exports = ['$log',  LoginController ];
+module.exports = ['$log', '$q', '$http', 'profileService', ProfileController ];
 
-function LoginController($log){
-  $log.debug('init LoginController');
+function ProfileController($log, $q, $http, profileService){
+  $log.debug('init LoginßController');
+
+  this.fetchProfile = function() {
+    profileService.fetchProfile()
+    .then((profile) => {
+      this.profile = profile;
+    })
+    .catch((err) => {
+      $log.error(err, err.message);
+      // return $q.reject(err);
+    });
+  };
+
+  this.fetchProfile();
 }
-
