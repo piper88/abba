8d9f2edf700da95a76520a14596169f83c37f19f
Logan Rogers <logan.s.rogers@biola.edu>
2016-11-15 10:59:45 -0800

    fixed functionality in landing controller

--- app/view/landing/landing-controller.js
+++ app/view/landing/landing-controller.js
@@ -1,10 +1,3 @@
 'use strict';
 
 require('./_landing.scss');
-
-module.exports = ['$log', '$rootScope', '$location', 'authService',  LandingController ];
-
-function LandingController($log, $rootScope, $location, authService){
-  $log.debug('init landingCtrl');
-
-}
