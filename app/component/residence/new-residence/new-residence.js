9690048c9b6ac778868d514741ad775ec48c3423
raziyeh bazargan <r.bazargan@gmail.com>
2016-11-17 12:11:36 -0800

    working on residence view page - add delete residence for residence view

--- app/component/residence/new-residence/new-residence.js
+++ app/component/residence/new-residence/new-residence.js
@@ -10,23 +10,45 @@ module.exports = {
 
 function NewResidenceController($log, $window, $rootScope, $location, authService, residenceService){
   $log.debug('init residenceCtrl');
-  this.newResidences = [];
-  
+  this.residences = [];
+  this.states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+
   this.createNewResidence = function(residence){
     $log.debug('init createNewResidences()');
-    //residence.bedrooms = [];
-    //residence.address = 'unique address';
+    console.log(residence);
     return residenceService.createResidence(residence)
-   .then( residence => {
-     this.newResidence = residence;
-     this.newResidences.push(residence);
-     console.log('newResidenc', this.newResidence);
+   .then( data => {
+     this.residences.unshift(data);
+     console.log('newResidenc', data);
    });
   };
+  this.fetchResidences = function() {
+    residenceService.fetchResidences()
+    .then(data => {
+      this.residences.push(data);
+    });
+  };
+
   this.getResidence = function(residenceData) {
     
     this.currentResidence = residenceData;
     console.log(this.currentResidence);
     return this.currentResidence;
   };
+
+  this.deleteResidence = function(residenceID) {
+    residenceService.deleteResidence(residenceID)
+    .then(()=> {
+      $log.debug('removed residence');
+      for(let i=0; i< this.residences.length; i++) {
+        if(residenceID === this.residences[i]._id)
+          this.residences.splice(i,1);
+      }
+    })
+    .catch(() => {
+      $log.debug('can not remove');
+    });
+  };
+
+  this.fetchResidences();
 }
