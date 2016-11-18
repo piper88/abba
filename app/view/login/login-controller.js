08e9fe45e284784f5138e18be890df32be24ae3d
Sarah DeBey <sarah.debey@gmail.com>
2016-11-17 23:53:44 -0800

    asdfasdf

--- app/view/login/login-controller.js
+++ app/view/login/login-controller.js
@@ -23,7 +23,7 @@ function LoginController($log, $rootScope, $location, authService){
   let googleAuthResponseType = 'response_type=code';
   let googleAuthClientID = `client_id=${__GOOGLE_CLIENT_ID__}`;
   let googleAuthScope = 'scope=profile%20email%20openid';
-  let googleAuthRedirectURI = 'redirect_uri=http://localhost:3000/api/auth/oauth_abba_callback';
+  let googleAuthRedirectURI = `redirect_uri=${__API_URL__}/api/auth/oauth_abba_callback`;
   let googleAuthAccessType = 'access_type=offline';
   let googleAuthPrompt = 'prompt=consent';
   this.googleAuthURL = `${googleAuthBase}?${googleAuthResponseType}&${googleAuthClientID}&${googleAuthScope}&${googleAuthRedirectURI}&${googleAuthAccessType}&${googleAuthPrompt}`;
